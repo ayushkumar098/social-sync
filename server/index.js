e2c8bc4439b4f9027f887af35320676788f8c607
Ayush Kumar <83111699+ayushkumar098@users.noreply.github.com>
2023-10-02 11:34:31 +0530

    Updated socket url in server index.js

--- server/index.js
+++ server/index.js
@@ -69,7 +69,7 @@ const server = app.listen(process.env.PORT, () =>
 // const server = createServer();
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://stalwart-moonbeam-2f40cd.netlify.app",
     credentials: true,
   },
 });
